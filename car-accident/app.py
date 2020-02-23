ce5ff81ce11c533e783c5c71d17521c1552f7c5f
Furkan Ozbek <furkanozbek1995@gmail.com>
2020-02-23 19:31:32 +0300

    app oluşturuldu. Genel yapı eklendi

---
+++ car-accident/app.py
@@ -0,0 +1,43 @@
+import cv2
+import numpy as np
+import argparse
+
+IMAGE_PATH="IMAGE_PATH"
+CFG_FILE="CFG_FILE"
+WEIGHT_FILE="WEIGHT_FILE"
+NAMES_FILE="NAMES_FILE"
+
+# handle command line arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('-i', '--image', required=True, help = 'path to input image')
+parser.add_argument('-c', '--config', required=True, help = 'path to yolo config file')
+parser.add_argument('-w', '--weights', required=True, help = 'path to yolo pre-trained weights')
+parser.add_argument('-n', '--names', required=False, help = 'path to yolo pre-trained weights')
+
+args = vars(parser.parse_args())
+
+options = {
+  IMAGE_PATH: args["image"],
+  CFG_FILE: args["config"],
+  WEIGHT_FILE: args["weights"],
+  NAMES_FILE: args["names"],
+}
+
+image_path = options[IMAGE_PATH]
+cfg_file = options[CFG_FILE]
+weight_file = options[WEIGHT_FILE]
+names_file = options[NAMES_FILE]
+
+print(image_path)
+print(cfg_file)
+print(weight_file)
+
+# Load network arch.
+# darknet = Darknet(cfg_file)
+
+# Load pre trained weights
+# darknet.load_weights(weight_file)
+
+# Load COCO obj classes
+# class_names = load_class_names(names_file)
+# print(darknet)
